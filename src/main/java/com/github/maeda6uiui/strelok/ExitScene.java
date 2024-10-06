fed216a1f1c40f03d882e6ef1a1dbd5f4f4ecc81
maeda6uiui <63230315+maeda6uiui@users.noreply.github.com>
2024-10-06 18:32:54 +0900

    Add ExitScene

---
+++ src/main/java/com/github/maeda6uiui/strelok/ExitScene.java
@@ -0,0 +1,15 @@
+package com.github.maeda6uiui.strelok;
+
+import com.github.maeda6uiui.mechtatel.core.MttWindow;
+
+/**
+ * Scene to exit the game
+ *
+ * @author maeda6uiui
+ */
+public class ExitScene implements IScene {
+    @Override
+    public void update(MttWindow window) {
+        window.close();
+    }
+}
